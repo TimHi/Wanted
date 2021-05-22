4f2f784dfbe3a4193de3afbc170bbdb01cdb49a8
TimHi <TimHiller@live.de>
2021-05-22 22:26:08 +0200

    adding config

---
+++ Services/DataService/FBIAPI/Config 2.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Services.DataService.FBIAPI
+{
+    public static class Config
+    {
+        public static string BaseURL = @"https://api.fbi.gov/";
+
+        public static string WantedEndpoint = @"@wanted/";
+    }
+}
